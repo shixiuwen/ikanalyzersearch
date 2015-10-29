cd25e346110aeef6049b6a682aff6d9f9dc228bc
ShiXiuwen <amos.shi@carbit.com.cn>
2015-10-29 17:24:27 +0800

    修改代码

--- src/main/java/com/example/shixiuwen/ikanalyzersearch/MainActivity.java
+++ src/main/java/com/example/shixiuwen/ikanalyzersearch/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.shixiuwen.ikanalyzersearch;
 
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
@@ -21,12 +19,19 @@ import java.io.StringReader;
 public class MainActivity extends ActionBarActivity {
 
     private EditText etSearch;
-    private Button btnSearch;
     private ListView lvResult;
     IKSegmenter ik;
     StringBuilder sb;
 
+    String s1 = "光谷软件创新科技产业园";
+    String s2 = "光谷产业园";
+    String s3 = "华中科技大学";
+    String s4 = "光谷软件园";
+    String s5 = "软件科技产业园";
+    String s6 = "光谷科技软件园";
 
+    //定义一个String数组存放待分割的字符串
+    private String[] searchResult = {s1,s2,s3,s4,s5,s6};
 
     int count01, count02, count03, count04, count05, count06;
 
@@ -37,16 +42,9 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        PackageManager manager = getPackageManager();
-
-        ApplicationInfo applicationInfo = manager.getApplicationInfo("", 0);
-        applicationInfo.targetSdkVersion;
-
         etSearch = (EditText) findViewById(R.id.id_et_des);
-        btnSearch = (Button) findViewById(R.id.id_btn_search);
+        Button btnSearch = (Button) findViewById(R.id.id_btn_search);
         lvResult = (ListView) findViewById(R.id.id_lv_result);
-
-        final String str = etSearch.getText().toString();
 
         btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,24 +52,13 @@ public class MainActivity extends ActionBarActivity {
                 //对目的地进行分词
                 destination = split(etSearch.getText().toString().trim());
                 //进行匹配排序
-                String[] sort = sort(destination);
-                lvResult.setAdapter(new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, sort));
+                String[] sort = sort(searchResult);
+                lvResult.setAdapter(new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, sort));
             }
         });
-
     }
 
-    public String[] sort(String[] split) {
-
-        String s1 = "光谷软件创新科技产业园";
-        String s2 = "光谷产业园";
-        String s3 = "华中科技大学";
-        String s4 = "光谷软件园";
-        String s5 = "软件科技产业园";
-        String s6 = "光谷科技软件园";
-
-        //定义一个String数组存放待分割的字符串
-        String[] strArray = {s1, s2, s3, s4, s5, s6};
+    public String[] sort(String[] strArray) {
 
         //定义一个二维数组存放分词结果数组
         String[][] array2D = new String[6][];
@@ -87,8 +74,8 @@ public class MainActivity extends ActionBarActivity {
         for (int arr2D = 0; arr2D < array2D.length; arr2D++) {
             if (destination != null) {
                 for (int i = 0; i < array2D[arr2D].length; i++) {
-                    for (int j = 0; j < destination.length; j++) {
-                        if (array2D[arr2D][i].equals(destination[j])) {
+                    for (String aDestination : destination) {
+                        if (array2D[arr2D][i].equals(aDestination)) {
                             count[arr2D]++;
                         }
                     }
@@ -99,29 +86,29 @@ public class MainActivity extends ActionBarActivity {
         //匹配计数完成，对结果进行排序,对计数排序的同时对选项字符数组同时进行了排序
         for (int i = 0; i < count.length; i++) {
             for (int j = i + 1; j < count.length; j++) {
-                int temp = 0;
-                String Stemp = null;
-                String[] SStemp = null;
+                int temp;
+                String Stems ;
+                String[] SStems;
                 if (count[i] < count[j]) {
                     //交换计数值的同时交换二维数组中字符串的顺序
                     temp = count[i];
-                    Stemp = strArray[i];
-                    SStemp = array2D[i];
+                    Stems = strArray[i];
+                    SStems = array2D[i];
                     count[i] = count[j];
                     strArray[i] = strArray[j];
                     array2D[i] = array2D[j];
                     count[j] = temp;
-                    strArray[j] = Stemp;
-                    array2D[j] = SStemp;
+                    strArray[j] = Stems;
+                    array2D[j] = SStems;
                 } else if (count[i] == count[j]) {
                     //如果计数相等，把匹配最短的字符串前置
                     if (array2D[i].length > array2D[j].length) {
-                        Stemp = strArray[i];
-                        SStemp = array2D[i];
+                        Stems = strArray[i];
+                        SStems = array2D[i];
                         strArray[i] = strArray[j];
                         array2D[i] = array2D[j];
-                        strArray[j] = Stemp;
-                        array2D[j] = SStemp;
+                        strArray[j] = Stems;
+                        array2D[j] = SStems;
                     }
                 }
             }
@@ -139,11 +126,11 @@ public class MainActivity extends ActionBarActivity {
         ik = new IKSegmenter(sr, true);
         sb = new StringBuilder();
         //开始分词
-        Lexeme lex = null;
+        Lexeme lex ;
         try {
             while ((lex = ik.next()) != null) {
                 //分词结果以空格分开
-                sb.append(lex.getLexemeText() + " ");
+                sb.append(lex.getLexemeText()).append(" ");
             }
             if (str.equals(etSearch.getText().toString().trim())) {
                 Toast.makeText(MainActivity.this, sb, Toast.LENGTH_SHORT).show();
